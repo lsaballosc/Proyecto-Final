c50c66e5a0ed07bf4ec2f9a73ad035e7d63e22e4
lsaballosc <102936989+lsaballosc@users.noreply.github.com>
2022-04-26 11:22:47 -0600

    Delete BlocDeNotas-master directory

--- BlocDeNotas-master/compile.bat
+++
@@ -1,7 +0,0 @@
-javac -d .\bin -cp .\bin .\src\principal\*.java
-xcopy .\src\imagenes .\bin\imagenes\ /E /Y
-copy .\manifest.mf .\bin\
-cd bin && jar cvfm BlocDeNotas.jar .\manifest.mf .\*
-cd ..
-
-java -jar bin\BlocDeNotas.jar
